5a8999cbf3502272545f073898e70ca5198c3def
unknown <yanghuning@imeik.net>
2021-12-25 18:26:57 +0800

    '锻炼系统初始化'

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,10 +3,6 @@ import { connect } from 'react-redux';
 
 import { HashRouter, NavLink, Route, Switch } from 'react-router-dom';
 
-import Dashboard from './dashboard';
-import Addto from './addto';
-import DataList from './datalist';
-
 import 'styles/home.less';
 
 import 'antd/dist/antd.css';
@@ -16,39 +12,7 @@ class Home extends Component {
         return (
             <div className="app-home">
 
-                <HashRouter>
-
-                    <div className="app-home-header">
-                        <NavLink className="nav-item" activeClassName="nav-active" to="/dashboard">Dashboard</NavLink>
-                        <NavLink className="nav-item" activeClassName="nav-active" to="/addto">添加</NavLink>
-                        <NavLink className="nav-item" activeClassName="nav-active" to="/datalist">列表</NavLink>
-                    </div>
-            
-                    <div className="app-home-body">
-
-                        <Switch>
-
-                            <Route exact path="/">
-                                <Dashboard />
-                            </Route>
-
-                            <Route path="/dashboard">
-                                <Dashboard />
-                            </Route>
-
-                            <Route path="/addto" tag="yhn">
-                                <Addto />
-                            </Route>
-
-                            <Route path="/datalist">
-                                <DataList />
-                            </Route>
-
-                        </Switch>
-
-                    </div>
-
-                </HashRouter>
+                锻炼系统初始化
                 
             </div>
         )
